f5005fa240cfcde01a663a3edf1c64c3194d11e6
Sage.Smith <sage.smith@workiva.com>
2016-10-20 20:33:43 -0600

    Finished navigation first pass

--- src/navigation.py
+++ src/navigation.py
@@ -1,3 +1,7 @@
+import directions
+import actions
+
+
 class Navigator:
 
     def __init__(self, reasoning_agent):
@@ -39,21 +43,26 @@ class Navigator:
             square to the desired square
         """
         frontier = list()
-        frontier.append((cur_x, cur_y))
+        frontier.append([cur_x, cur_y, cur_dir, []])
         came_from = dict()
         came_from[(cur_x, cur_y)] = None
 
         while len(frontier) > 0:
-            current = frontier.pop()
-            if current == (fin_x, fin_y):
-                break
-            x, y = current
-            to_visit = self.get_neighbors(x, y)
-            for loc in to_visit:
+            x, y, direction, actions_to_take = frontier.pop()
+            if (x, y) == (fin_x, fin_y):
+                return actions_to_take
+
+            to_visit = self.calculate_actions(
+                x, y, direction, actions_to_take, self.get_neighbors(x, y)
+            )
+
+            to_visit.sort(key=lambda k: len(k[3]))
+            for cell in to_visit:
+                loc = cell[0], cell[1]
                 if loc not in came_from:
-                    frontier.append(loc)
-                    came_from[loc] = current
-        return current
+                    frontier.append(cell)
+                    came_from[loc] = cell
+        return None
 
     def get_neighbors(self, x, y):
         """
@@ -67,5 +76,47 @@ class Navigator:
         :return: list of places to check
         """
         return filter(
-            self.reasoning_agent, [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
+            self.reasoning_agent, [[x+1, y], [x-1, y], [x, y+1], [x, y-1]]
         )
+
+    def calculate_actions(self, x, y, direction, actions_to_take, to_visit):
+        loc = [x, y]
+        direction_vec = directions.MOVEMENTS[direction]
+
+        for cell in to_visit:
+            next_direction_vec = [cell[0] - loc[0], cell[1] - loc[1]]
+            next_direction = directions.VECTORS[tuple(next_direction_vec)]
+            num_moves = max(
+                direction_vec[0]+next_direction_vec[0],
+                direction_vec[1]+next_direction_vec[1]
+            )
+            cell.append(next_direction)
+            cell.append(self.resolve_actions(
+                    actions_to_take, direction,
+                    next_direction, num_moves
+                )
+            )
+
+        return to_visit
+
+    def resolve_actions(
+            self, actions_to_take, direction, next_direction, num_moves):
+        if num_moves == 1:
+            # Bridger maths...
+            left = (
+                abs(direction-next_direction) % 4 <
+                abs(direction-next_direction-4) % 4
+            )
+            return (
+                actions_to_take +
+                ([actions.LEFT] if left else [actions.RIGHT]) +
+                [actions.FORWARD]
+            )
+
+        if num_moves == 2:
+            return actions_to_take+[
+                actions.LEFT,
+                actions.LEFT,
+                actions.FORWARD
+            ]
+        return actions_to_take + [actions.FORWARD]
