ec6b30d1d832a71874d725a2579da54671781c69
bridger.howell <d25w197@bz-cs-cslab15l.msu.montana.edu>
2016-10-21 07:35:56 -0600

    Add initial reasoning system; needs style check up and separating apart

---
+++ src/node.py
@@ -0,0 +1,198 @@
+ast_types = range(3)
+CONST, VAR, FUNC = ast_types
+
+
+class Node:
+    """
+    A syntax node representing at most a term in a disjunction.
+    """
+    def __init__(self, type, name, args=None):
+        """
+        Creates a normal syntax node.
+
+        :param type: the kind of node being created
+        :type type: int
+        :param name: the name of the node
+        :type name: string or int
+        :param args: extra argument(s) needed by the node
+        :type param: list[Node], int, or None (depending of node type)
+        """
+        self.type = type
+        self.name = name
+        self.args = args
+
+    def __repr__(self):
+        """
+        A basic representation of a node for debugging.
+        """
+        return '{}:{} [{}]'.format(self.type, self.name, self.args)
+
+    def __eq__(self, other):
+        return self.type == other.type and self.name == other.name and self.args == other.args
+
+    def __hash__(self):
+        if type(self.args) == list:
+            return hash((self.type, self.name, tuple(self.args)))
+        return hash((self.type, self.name, self.args))
+    
+    def replace(self, var_name, node):
+        """
+        Constructs a new node with any variables with the given
+        name in the specified node replaced with the given
+        replacement node.
+
+        :param var_name: the name of the variable to replace
+        :type var_name: string
+        :param node: the node to replace the specified variable
+        :type node: Node
+        :rtype: Node
+        :returns: a node with the given variable replaced by a
+            specific node.
+        """
+        if self.type == VAR and self.name == var_name:
+            if self.args != 0:
+                if node.type == VAR:
+                    return var(node.name, self.args + node.args)
+                if node.type == CONST:
+                    return var(node.name + self.args)
+                else:
+                    raise Exception("Can not offset a function")
+            return node
+        if self.type == FUNC:
+            return func(self.name, [arg.replace(var_name, node) for arg in self.args])
+        return self
+    
+def const(name):
+    """
+    Creates a node representing a constant.
+
+    :param name: name or numeric value of the constant
+    :type name: string or int (depending on the kind of constant)
+    :rtype: Node
+    :returns: a node representing the given constant
+    """
+    return Node(CONST, name)
+
+def var(name, offset=0):
+    """
+    Creates a node representing a variable, potentially having 
+    an arithmetic offset. 
+    (e.g. var(x, offset=1) represents X+1)
+    
+    :param name: the variable's name
+    :type name: string
+    :param offset: the variable's arithmetic offset
+    :type offset: int
+    :rtype: Node
+    :returns: a node reprsenting a variable 
+    """
+    return Node(VAR, name, offset)
+
+def func(name, args):
+    """
+    Creates a node representing a function call, having as
+    many arguments as necessary.
+
+    :param name: the function's name
+    :type name: string
+    :param args: the parameters to the function
+    :type args: list[Node]
+    :rtype: Node
+    :returns: a node reprsenting a function call
+    """
+    return Node(FUNC, name, args)
+
+def unify(node1, node2):
+    """
+    Unfies two syntax nodes and gives the substitution string
+    which unfies the two nodes.
+
+    :param node1: the first node to unify
+    :type node1: Node
+    :param node2: the second node to unify
+    :type node2: Node
+    :rtype: map{str: Node}
+    :returns: a map from variable names to the nodes to replace 
+        those variables, or None if the two terms can't unify.
+    """
+    return unify_str(node1, node2, {})
+
+def unify_str(node1, node2, subs):
+    """
+    Recursively unfies two syntax nodes having already processed
+    the given substitutions.
+
+    :param node1: the first node to unify
+    :type node1: Node
+    :param node2: the second node to unify
+    :type node2: Node
+    :rtype: map{str: Node}
+    :returns: a substitution map from variable names to the nodes 
+        to replace those variables, or None if the two terms don't
+        unify.
+    """
+    if node1.type == CONST and node2.type == CONST:
+        if node1.name == node2.name:
+            return subs
+        return None
+    if node1.type == VAR:
+        return unify_var(node1, node2, subs)
+    if node2.type == VAR:
+        return unify_var(node2, node1, subs)
+    if node2.type == FUNC and node2.type == FUNC:
+        if node1.name != node2.name:
+            return None
+        if len(node1.args) != len(node2.args):
+            return None
+
+        for i in xrange(len(node1.args)):
+            res = unify_str(node1.args[i], node2.args[i], subs)
+            if res is None:
+                return None
+        return subs
+    return None
+
+def unify_var(variable, other, subs):
+    """
+    Recursively unfies two syntax nodes, one of which is known
+    to be a variable, having already processed the given
+    substitutions.
+
+    This factors in some edge cases for variable offset arith-
+    matic, and variables already being unified.
+
+    Does not include the occurs check (but it really should).
+
+    :param variable: the variable to unify
+    :type variable: Node
+    :param other: the other node to unify with
+    :type other: Node
+    :returns: a substitution map
+    """
+    if variable.name in subs:
+        sub_term = sub[variable.name]
+        if sub_term.type == VAR:
+            return unify_str(var(sub_term.name, variable.args + sub_term.args), other, subs)
+        else:
+            return unify_str(sub_term, other, subs)
+    if other.type == VAR and other.name in subs:
+        sub_term = sub[other.name]
+        if sub_term.type == VAR:
+            return unify_str(variable, var(sub_term.name, other.args + sub_term.args), subs)
+        else:
+            return unify_str(sub_term, other, subs)
+    if variable.args != 0:
+        if other.type == CONST:
+            if type(other.name) != int:
+                return None
+            subs[variable.name] = const(other.name - variable.args)
+            return subs
+        elif other.type == FUNC:
+            return None
+
+    if other.type == VAR:
+        subs[variable.name] = var(other.name, other.args - variable.args)
+        subs[other.name] = var(variable.name, variable.args - other.args)
+    else:
+        subs[variable.name] = other        
+    return subs
---
+++ src/reasoning_system.py
@@ -0,0 +1,205 @@
+import itertools
+import node
+
+class ReasoningSystem:
+    def __init__(self, axioms):
+        """
+        Creates a new reasoning system that can store axioms and
+        perform resolution to try to prove queries.
+        
+        :param axioms: the initial axioms for the system
+        :type axioms: list[Clause]
+        """        
+        self.facts = list(axioms)
+
+    def tell(self, fact):
+        """
+        Adds a fact to the knowledge base, to be used when
+        proving later queries.
+
+        :param fact: the fact to add
+        :type fact: Clause
+        """
+        self.facts.append(fact)
+
+    def ask(self, query):
+        """
+        Tries to prove a given query based on stored knowledge
+        by applying resolution.
+
+        :rtype: bool
+        :returns: whether or not the query can be proven
+        """
+        return resolution(self.facts, query)
+
+
+class Clause:
+    def __init__(self, pos, neg):
+        """
+        Creates a new disjunctive clause separated into positive
+        and negative terms.
+
+        :param pos: the non-negated terms in the clause
+        :type pos: list[Node]
+        :param neg: the negated terms in the clause
+        :type neg: list[Node]
+        """
+        self.pos = pos
+        self.neg = neg
+
+    def empty(self):
+        """
+        Determines whether this clause is the empty clause (that
+        is, whether it has no terms).
+
+        :rtype: bool
+        :returns: whether or not this clause is empty
+        """
+        return len(self.pos) == 0 and len(self.neg) == 0
+
+    def __repr__(self):
+        """
+        Gives a simple representation of the Clause
+        """
+        return repr(self.pos) + " > " + repr(self.neg) 
+
+    def __eq__(self, other):
+        return self.pos == other.pos and self.neg == other.neg
+
+    def __hash__(self):
+        return hash((tuple(self.pos), tuple(self.neg)))
+    
+def substitute_term(sub_str, term):
+    """
+    Create a new Node by applying a substitution string over its
+    entire tree structure.
+    """
+    for var_name in sub_str.iterkeys():
+        term = term.replace(var_name, sub_str[var_name])
+
+    return term
+
+def resolution(clauses, query):
+    """
+    Tries to prove a given query by contradiction by repeatedly
+    applying resolution in stages. So, first the query is assumed
+    to be false.
+
+    If a contradicion is reached, the query is proved.
+
+    If no new clauses are found, at some point, then the query
+    cannot be proved from the current knowledge base.
+
+    :param clauses: the basic knowledge of the system
+    :type clauses: list[Clause]
+    :param query: the query to try to prove
+    :type query: Clause
+    :rtype: bool
+    :returns: whether or not the query can be proved from the
+        provided facts.
+    """
+    clauses = set(clauses)
+    clauses.add(Clause([], [query]))
+
+    while True:
+        new = set()
+        for clause_pair in itertools.combinations(clauses, 2):
+            resolvents = resolve(*clause_pair)
+            if resolvents:
+                if resolvents.empty():
+                    return True
+                new |= {resolvents}
+        if new <= clauses:
+            return False
+        clauses |= new
+        
+
+def resolve(clause_one, clause_two):
+    """
+    Resolves the two clauses, return a new clause with variables subsituted
+    and disjuncts removed according to their unification.
+
+    Specifically, if any positive disjunct unfies with a negative disjunct
+    from the other clause, both are removed from the result and the substit-
+    ution string is applied to the final result.
+
+    :param clause_one: the first clause to resolve
+    :type clause_one: Clause
+    :param clause_two: the other clause to resolve
+    :type clause_two: Clause
+    :rtype: Clause
+    :returns: the Clause resulting from performing resolution on the 
+    """
+    sub_str = {}
+    new_pos = []
+    new_neg = []
+
+    resolve_with(clause_one.pos, clause_two.neg, sub_str, new_pos, new_neg)
+    resolve_with(clause_two.pos, clause_one.neg, sub_str, new_pos, new_neg)
+
+    if len(new_neg) == len(clause_one.neg) + len(clause_two.neg):
+        return None
+
+    new_neg = [substitute_term(sub_str, term) for term in new_neg]
+    new_pos = [substitute_term(sub_str, term) for term in new_pos]    
+    
+    return Clause(new_pos, new_neg)
+
+def resolve_with(pos, neg, sub_str, new_pos, new_neg):
+    """
+    Resolves the positive terms from one clause with the negative terms
+    of another clause, updating the subsitution string and adding
+    any terms that aren't resolved to the new clause.
+
+    :param pos: the positive disjuncted terms from one clause
+    :type pos: list[Node]
+    :param neg: the negative disjuncted terms from the other clause
+    :type neg: list[Node]
+    :param sub_str: the substitution string to update
+    :type sub_str: map{string: Node}
+    :param new_pos: the positive disjuncted terms to be in the resulting clause
+    :type new_pos: list[Node]
+    :param new_pos: the negative disjuncted terms to be in the resulting clause
+    :type new_neg: list[Node]
+    """
+    untouched_neg = set(neg)
+    
+    for pterm in pos:
+        resolved_term = False
+        for nterm in neg:
+            nterm = substitute_term(sub_str, nterm)
+            subs = node.unify(pterm, nterm)
+
+            if subs is not None:
+                pterm = substitute_term(subs, pterm)
+                sub_str.update(subs)
+                resolved = resolved_term = True
+                untouched_neg.remove(nterm)
+        if not resolved_term:
+            new_pos.append(pterm)
+
+    new_neg.extend(list(untouched_neg))
+
+
+if __name__ == '__main__':
+    """
+    A simple test of reasoning system.
+
+    Initial knowledge:
+     -f(x) :- g(x)
+
+    Told:
+     -g(5)
+
+    Tested:
+     -can prove f(5)
+     -can't prove f(4)
+    """
+    from node import *
+    
+    r = Clause([func('f', [var('x')])], [func('g', [var('x')])])
+    rs = ReasoningSystem([r])
+    rs.tell(Clause([func('g', [const(5)])], []))
+    
+    print rs.ask(func('f', [const(5)]))
+    print rs.ask(func('f', [const(4)]))    
