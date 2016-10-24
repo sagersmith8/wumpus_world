b249c45a831ebb2250056ccc9dc1eaf3173bd34d
JWhitney <jwjwhitney@gmail.com>
2016-10-24 09:21:53 -0600

    added out.table, and paranthesees around prints

--- src/table_shower.py
+++ src/table_shower.py
@@ -5,7 +5,7 @@ algorithms = ['Reactive Agent']
 
 table_format = {
     'sizes': [5, 10, 15, 20, 25],
-    'prob_total': [0.6, 0.8, 0.9],
+    'prob_total': [0.2, 0.4, 0.6, 0.8],
     'sub_probs': [
         'High Obstacle',
         'High Pit',
@@ -46,12 +46,12 @@ for algorithm in algorithms:
         rows.append(row)
 
         
-    print '\\subsection{'+algorithm+' Results}\n'
+    print ('\\subsection{'+algorithm+' Results}\n')
     for row_num, row in enumerate(rows):
-        print '\\begin{table}[ht]\n\\centering\n\\resizebox{\\textwidth}{!}{\\begin{tabular}{c|c|c|c|c|c|c|c|c|}\n\\cline{2-9}\n & prob & HO & HP & HW & EQ & NO & NP & NW \\\\\n\\hline\n\\multirow{3}{*}{percent completed}'
-        print '&' + '& '.join(str(table_format['prob_total'][pindex])+' & ' + ' & '.join(str(i['finished%']) for i in p)+'\\\\\n' for pindex, p in enumerate(row))+'\n\\hline'
+        print ('\\begin{table}[ht]\n\\centering\n\\resizebox{\\textwidth}{!}{\\begin{tabular}{c|c|c|c|c|c|c|c|c|}\n\\cline{2-9}\n & prob & HO & HP & HW & EQ & NO & NP & NW \\\\\n\\hline\n\\multirow{4}{*}{percent completed}')
+        print ('&' + '& '.join(str(table_format['prob_total'][pindex])+' & ' + ' & '.join(str(i['finished%']) for i in p)+'\\\\\n' for pindex, p in enumerate(row))+'\n\\hline')
         
         for stat in entry_order:
-            print ('\multirow{3}{*}{' + stat.replace('_',' ') + '}&' +'\\\\\n& '.join(str(table_format['prob_total'][pindex]) + ' & ' + ' & '.join(pm(i[stat]) for i in p)+'' for pindex, p in enumerate(row))+'\\\\\n\\hline\n')
+            print ('\multirow{4}{*}{' + stat.replace('_',' ') + '}&' +'\\\\\n& '.join(str(table_format['prob_total'][pindex]) + ' & ' + ' & '.join(pm(i[stat]) for i in p)+'' for pindex, p in enumerate(row))+'\\\\\n\\hline\n')
         print('\\end{tabular}}\n\\caption{' + algorithm + ': size ' + str((row_num+1)*5) + '}\n\\end{table}\n\n')
 
